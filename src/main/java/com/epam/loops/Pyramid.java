e94f3e8ba26525d22f2ccd25c8e6d0750516994f
EDyugaeva <ed2408@yandex.ru>
2023-01-05 18:04:15 +0500

    commit

--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -3,17 +3,19 @@ package com.epam.loops;
 public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
-        for (int i = 0; i < cathetusLength; i++) {
-            for (int j = 0; j < cathetusLength*2 - 1; j++) {
-                if (j == cathetusLength - 1) {
-                    System.out.print(1);
-                }
-                else if (j >= Math.abs(cathetusLength- i - 1) && j < cathetusLength) {
-                    System.out.print(Math.abs(j - cathetusLength));
-                }
-                else if (j <= Math.abs(cathetusLength+i-1)&& j >= cathetusLength) {
-                    System.out.print(Math.abs(j - cathetusLength+2));
-                }else {
+        for (int i = 1; i <= cathetusLength; i++) {
+            int temp = cathetusLength - i;
+            int temp2 = cathetusLength + i;
+
+            for (int j = 1; j < cathetusLength * 2; j++) {
+                if (j > temp && j <= cathetusLength) {
+                    System.out.print(cathetusLength - j + 1);
+                } else if (j >= cathetusLength && j < temp2) {
+                    System.out.print(j - cathetusLength + 1);
+
+                } else if (j > cathetusLength + i - 1) {
+                    continue;
+                }else{
                     System.out.print(" ");
                 }
             }
